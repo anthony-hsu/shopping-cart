4c7a96735d53d3132d658b8637665256539fcbb2
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-07-15 22:24:47 -0700

    Add some formatting for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,22 +2,24 @@ import React, { useState } from "react";
 
 function Card(props) {
   const [quantity, setQuantity] = useState(0);
-  const { cartItems, setCartItems } = props;
+  const { cartItems, setCartItems, title, imageUrl } = props;
   const addProduct = () => {
     setCartItems(cartItems + quantity);
   };
   return (
     <div className="card">
-      <div className="div-product-image"></div>
-      <div>
+      <img src={imageUrl} alt={title} className="img-product" />
+      <p className="p-product-title">{title}</p>
+      <div className="div-add-item">
         <input
+          className="input-add"
           type="number"
           value={quantity}
           onChange={(e) => setQuantity(parseInt(e.target.value))}
           onFocus={(e) => e.target.select()}
         />
         <button className="btn-add" onClick={addProduct}>
-          Add
+          Add To Cart
         </button>
       </div>
     </div>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,18 +1,72 @@
-import React, { useState } from "react";
+// import { useState } from "react";
 import Card from "./Card";
 import uniqid from "uniqid";
 
 function Shop(props) {
   const { cartItems, setCartItems } = props;
-  const [cards, setCards] = useState([
-    { id: uniqid() },
-    { id: uniqid() },
-    { id: uniqid() },
-  ]);
+  const cards = [
+    {
+      id: uniqid(),
+      title: "Apple",
+      imageUrl:
+        "https://www.applesfromny.com/wp-content/uploads/2020/06/SnapdragonNEW.png",
+    },
+    {
+      id: uniqid(),
+      title: "Pear",
+      imageUrl:
+        "https://img.imageboss.me/fourwinds/width/425/dpr:2/shop/products/shutterstock_1693201075.png?v=1621966032",
+    },
+    {
+      id: uniqid(),
+      title: "Strawberry",
+      imageUrl:
+        "https://www.thermofisher.com/blog/food/wp-content/uploads/sites/5/2015/08/single_strawberry__isolated_on_a_white_background.jpg",
+    },
+    {
+      id: uniqid(),
+      title: "Watermelon",
+      imageUrl:
+        "https://i5.walmartimages.com/asr/14487aaf-d86a-4b46-acaf-7621b90286bb.fb737768267fcdc95c33f355b730ad15.jpeg",
+    },
+    {
+      id: uniqid(),
+      title: "Peach",
+      imageUrl: "https://glycemic-index.net/images/FVRSYndNy8.webp",
+    },
+    {
+      id: uniqid(),
+      title: "Grapes",
+      imageUrl:
+        "https://static.libertyprim.com/files/varietes/red-globe-large.jpg?1588355080",
+    },
+    {
+      id: uniqid(),
+      title: "Kiwi",
+      imageUrl:
+        "https://www.theproducemoms.com/wp-content/uploads/2022/01/kiwi-1024x1024.png",
+    },
+    {
+      id: uniqid(),
+      title: "Lemon",
+      imageUrl: "https://images.heb.com/is/image/HEBGrocery/000375185-1",
+    },
+  ];
+  // const [cards, setCards] = useState([
+  //   { id: uniqid() },
+  //   { id: uniqid() },
+  //   { id: uniqid() },
+  // ]);
   return (
-    <div>
+    <div className="div-shop">
       {cards.map((card) => (
-        <Card key={card.id} setCartItems={setCartItems} cartItems={cartItems} />
+        <Card
+          key={card.id}
+          setCartItems={setCartItems}
+          cartItems={cartItems}
+          title={card.title}
+          imageUrl={card.imageUrl}
+        />
       ))}
     </div>
   );
